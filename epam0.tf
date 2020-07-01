e41caa036234f43053e69315e95ff166d28dc177
ppjawsepam <67658257+ppjawsepam@users.noreply.github.com>
2020-07-01 18:55:56 +0200

    Update epam0.tf
    delete main vpc, add vpc1, vpc2

--- epam0.tf
+++ epam0.tf
@@ -1,8 +1,16 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "dedicated"
-
+resource "aws_vpc" "vpc1" {
+  region = "eu-central-1"
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "mainvpc"
+    Name = "vpc1"
   }
 }
+
+resource "aws_vpc" "vpc2" {
+  cidr_block = "10.10.0.0/16"
+  region = "eu-west-1"
+  tags = {
+    Name = "vpc2"
+  }
+}
+
