df6471c3300d905c52e9f0cecd21535e5ee0cec8
ppjawsepam <67658257+ppjawsepam@users.noreply.github.com>
2020-07-01 21:05:05 +0200

    Update epam0.tf
    vpc, SC1, EC2

--- epam0.tf
+++ epam0.tf
@@ -1,8 +1,38 @@
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
 resource "aws_vpc" "vpc1" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "vpc1"
   }
+ resource "aws_security_group" "SG1" {
+  name        = "SecuGrp1"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+} 
+  
 }
 
 resource "aws_vpc" "vpc2" {
@@ -12,3 +42,9 @@ resource "aws_vpc" "vpc2" {
   }
 }
 
+resource aws_instance "bastion" {
+  instance_type = "t2.micro"
+  ami = "ami-173c6d52"
+  tags {
+    name ="bastion" }
+  }
