dd7920aabb94111b7ec543cb1a751ce175eb1509
ppjawsepam <67658257+ppjawsepam@users.noreply.github.com>
2020-07-01 19:00:00 +0200

    Update epam0.tf
    delete region argument

--- epam0.tf
+++ epam0.tf
@@ -1,5 +1,4 @@
 resource "aws_vpc" "vpc1" {
-  region = "eu-central-1"
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "vpc1"
@@ -8,7 +7,6 @@ resource "aws_vpc" "vpc1" {
 
 resource "aws_vpc" "vpc2" {
   cidr_block = "10.10.0.0/16"
-  region = "eu-west-1"
   tags = {
     Name = "vpc2"
   }
