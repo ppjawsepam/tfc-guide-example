87b9204b58570b496ba2481fee6d863e6a8920e6
ppjawsepam <67658257+ppjawsepam@users.noreply.github.com>
2020-07-01 22:48:23 +0200

    Update epam0.tf
    remove sc

--- epam0.tf
+++ epam0.tf
@@ -5,31 +5,6 @@ resource "aws_vpc" "vpc1" {
   }
  } 
  
-resource "aws_security_group" "SG1" {
-  name        = "SecuGrp1"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = "vpc-02171481185a55f3a"
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 0
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh"
-  }
-  }
-
 resource "aws_vpc" "vpc2" {
   cidr_block = "10.10.0.0/16"
   tags = {
